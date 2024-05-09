9513ebd61171fc9e78982d4168cd45459f6c8831
mbovero <milesbovero@icloud.com>
2024-05-09 13:19:16 -0600

    Implementing configuration file compatibility

--- src/main/java/mcmgnetwork/mcmg_networkhandler/MCMG_NetworkHandler.java
+++ src/main/java/mcmgnetwork/mcmg_networkhandler/MCMG_NetworkHandler.java
@@ -8,18 +8,27 @@ import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import com.velocitypowered.api.proxy.server.ServerPing;
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import mcmgnetwork.mcmg_networkhandler.protocols.ChannelNames;
 import mcmgnetwork.mcmg_networkhandler.protocols.MessageTypes;
 import org.slf4j.Logger;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 @Plugin(
@@ -36,6 +45,7 @@ public class MCMG_NetworkHandler {
 
     private final ProxyServer proxy;
     private final Logger logger;
+    private static YamlDocument config;
 
     /**
      * Creates a new MCMG_NetworkHandler plugin instance, injecting a proxy server and logger for plugin capabilities
@@ -44,10 +54,31 @@ public class MCMG_NetworkHandler {
      * @param logger The logger used to write info, warnings, etc. to the server console
      */
     @Inject
-    public MCMG_NetworkHandler(ProxyServer proxy, Logger logger)
+    public MCMG_NetworkHandler(ProxyServer proxy, Logger logger, @DataDirectory Path dataDirectory)
     {
         this.proxy = proxy;
         this.logger = logger;
+
+        try
+        {
+            config = YamlDocument.create(new File(dataDirectory.toFile(), "config.yml"),
+                    Objects.requireNonNull(getClass().getResourceAsStream("/config.yml")),  // Get config from resources folder & populate defaults into config file
+                    GeneralSettings.DEFAULT,
+                    LoaderSettings.builder().setAutoUpdate(true).build(), // Config file will update automatically without user interaction
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder().setVersioning(new BasicVersioning("file-version"))    // Set route to config.yml for automatic versioning
+                            .setOptionSorting(UpdaterSettings.OptionSorting.SORT_BY_DEFAULTS).build()
+            );
+
+            config.update();
+            config.save();
+        } catch (IOException ex)
+        {
+            logger.error("Could not create/load plugin config! Plugin shutting down...");
+            // Shutdown plugin executor
+            Optional<PluginContainer> container = proxy.getPluginManager().getPlugin("mcmg-network-handler");
+            container.ifPresent(pluginContainer -> pluginContainer.getExecutorService().shutdown());
+        }
     }
 
     /**
