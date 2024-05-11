cbd9085892f1f4a7fc9769a76b495456de8c4db5
mbovero <milesbovero@icloud.com>
2024-05-11 16:53:08 -0600

    Renamed file and extracted server initialization logic into ServerInitializeUtil

--- src/main/java/mcmgnetwork/mcmg_networkhandler/utilities/ActiveServerUtil.java
+++ src/main/java/mcmgnetwork/mcmg_networkhandler/utilities/ActiveServerUtil.java
@@ -14,18 +14,19 @@ import java.util.concurrent.CompletableFuture;
 
 /**
  * Description: <p>
- *  A utility class holding static data and functions that handle specific actions and information regarding network
- *  servers.
+ *  A utility class holding static data and functions that handle specific actions and information regarding active
+ *  network servers.
  *
  *  <p>Author(s): Miles Bovero
  *  <p>Date Created: 5/9/24
  */
-public class ServerUtil
+public class ActiveServerUtil
 {
 
     /**
      * A map of the names of active servers and the ServerInfoPackage corresponding to that server
      */
+    @Getter
     private static final HashMap<String, ServerInfoPackage> activeServerInfo = new HashMap<>();
 
     /**
@@ -103,57 +104,4 @@ public class ServerUtil
 
         return targetServer;
     }
-
-
-    public static String startNewServer(String serverType)
-    {
-        // Attempt to retrieve a new server instance's name
-        String newServerName = getNewServerName(serverType);
-
-        // If there is no room for a new server of the specified type, return early
-        if (newServerName.isEmpty())
-            return ServerStatuses.FULL;
-
-        // Otherwise, create a new server with the new server name
-        createNewServerFile(newServerName);
-
-        //TODO change/remove
-        return ServerStatuses.INITIALIZING;
-    }
-
-    /**
-     * Checks if there is an available slot for a new server of the specified type and returns its name or an empty
-     * string if no slot is available.
-     * @param serverType The type of server to try to retrieve a new instance's name of
-     * @return The new server instance's name; or an empty string if there is no room for a new server of the specified
-     * type
-     */
-    private static String getNewServerName(String serverType)
-    {
-        // Find and store names of all active servers of the provided type
-        Set<String> activeServerNames = new HashSet<>();
-        for (String serverName : activeServerInfo.keySet())
-            if (serverName.contains(serverType))
-                activeServerNames.add(serverName);
-
-        // Iterate over active servers to try and find an open slot for a new server to exist
-        int maxServerTypeCount = ConfigManager.getMaxServerTypeCount(serverType);
-        for (int i=0; i<maxServerTypeCount; i++)
-        {
-            String serverName = serverType + i;
-            if (!activeServerNames.contains(serverName))
-                return serverName;
-        }
-
-        // If all possible servers of the specified type are full, return empty string
-        return "";
-    }
-
-    //TODO method header
-    private static void createNewServerFile(String newServerName)
-    {
-        Path currentDirectory = Paths.get("").toAbsolutePath(); // Get the current working directory of the Java process
-        MCMG_NetworkHandler.getLogger().info("Server creation file directory: " + currentDirectory.toString());
-    }
-
 }
