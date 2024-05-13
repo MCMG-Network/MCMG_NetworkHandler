407acd6f0424b0f36122cb113f5c944eda99364a
mbovero <milesbovero@icloud.com>
2024-05-12 19:03:25 -0600

    Moved functions related to server transferring to a new ServerTransferHandler class

--- src/main/java/mcmgnetwork/mcmg_networkhandler/PluginMessageHandler.java
+++ src/main/java/mcmgnetwork/mcmg_networkhandler/PluginMessageHandler.java
@@ -1,21 +1,14 @@
 package mcmgnetwork.mcmg_networkhandler;
 
 import com.google.common.io.ByteArrayDataInput;
-import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
 import lombok.Getter;
 import mcmgnetwork.mcmg_networkhandler.protocols.ChannelNames;
 import mcmgnetwork.mcmg_networkhandler.protocols.MessageTypes;
-import mcmgnetwork.mcmg_networkhandler.protocols.ServerStatuses;
-import mcmgnetwork.mcmg_networkhandler.utilities.ActiveServerUtil;
-import mcmgnetwork.mcmg_networkhandler.utilities.ServerInitializeUtil;
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Description: <p>
@@ -47,58 +40,8 @@ public class PluginMessageHandler
         ByteArrayDataInput in = ByteStreams.newDataInput(e.getData());
         String subChannel = in.readUTF();
 
-        // Only handle SERVER_TRANSFER_REQUESTs
+        // Only handle specific sub-channels / message types
         if (subChannel.equals(MessageTypes.LOBBY_TRANSFER_REQUEST))
-            handleLobbyTransferRequest(in);
-    }
-
-    //TODO add method header
-    private static void handleLobbyTransferRequest(ByteArrayDataInput in) {
-        // Read/store remaining plugin message data
-        String playerName = in.readUTF();
-        String serverType = in.readUTF();
-
-        // Get updated server information
-        CompletableFuture<Void> serverInfoFuture = ActiveServerUtil.getServerInfo();
-
-        // Wait for all server pings to complete, then run remaining code:
-        serverInfoFuture.thenRun(() ->
-        {
-            // Track server status (initially assumed to be online & transferable)
-            String serverStatus = ServerStatuses.TRANSFERABLE;
-
-            // Attempt to identify a target server to transfer the player to
-            String serverName = ActiveServerUtil.findTransferableServerName(serverType);
-
-            // If no transferable server could be found, attempt to start a new one
-            if (serverName.isEmpty())
-            {
-                serverStatus = ServerInitializeUtil.startNewServer(serverType);   // Store updated server status
-            }
-
-            // Send a response to the network
-            sendLobbyTransferResponse(serverStatus, playerName, serverName);
-        });
-    }
-
-    /**
-     * Sends a LOBBY_TRANSFER_RESPONSE with the provided parameters to the network using plugin messaging.
-     * @param serverStatus The ServerStatus of the lobby server type provided in the request
-     * @param playerName The name of the player requested to be transferred
-     * @param serverName The server instance name to transfer the specified player to
-     */
-    private static void sendLobbyTransferResponse(String serverStatus, String playerName, String serverName)
-    {
-        // Format return message
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF(MessageTypes.LOBBY_TRANSFER_RESPONSE);
-        out.writeUTF(serverStatus);
-        out.writeUTF(playerName);
-        out.writeUTF(serverName);
-        // Send response message
-        for (RegisteredServer server : MCMG_NetworkHandler.getProxy().getAllServers())
-            server.sendPluginMessage(MCMG_IDENTIFIER, out.toByteArray());
-
-        MCMG_NetworkHandler.getLogger().info("The MCMG_NetworkHandler is returning the requested server's status."); //TODO remove
+            ServerTransferHandler.handleLobbyTransferRequest(in);
     }
 }
