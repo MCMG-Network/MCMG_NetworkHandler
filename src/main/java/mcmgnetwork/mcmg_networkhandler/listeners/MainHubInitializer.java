10f4b7d4c8ef34094a1c99ba5e72a3c1ec48d374
mbovero <milesbovero@icloud.com>
2024-05-12 23:48:24 -0600

    Completed first implementation of automatic main hub initialization

--- src/main/java/mcmgnetwork/mcmg_networkhandler/listeners/MainHubInitializer.java
+++ src/main/java/mcmgnetwork/mcmg_networkhandler/listeners/MainHubInitializer.java
@@ -2,35 +2,37 @@ package mcmgnetwork.mcmg_networkhandler.listeners;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.*;
-import mcmgnetwork.mcmg_networkhandler.MCMG_NetworkHandler;
+import mcmgnetwork.mcmg_networkhandler.protocols.ServerTypes;
+import mcmgnetwork.mcmg_networkhandler.utilities.ActiveServerUtil;
+import mcmgnetwork.mcmg_networkhandler.utilities.ServerInitializeUtil;
+
+import java.util.concurrent.CompletableFuture;
 
 public class MainHubInitializer
 {
-    @Subscribe
-    public void onConnectionHandshakeEvent(ConnectionHandshakeEvent e)
-    {
-        MCMG_NetworkHandler.getLogger().info("ConnectionHandshakeEvent detected! {}", e.getIntent().toString());
-
-    }
-
-    @Subscribe
-    public void onLoginEvent(LoginEvent e)
-    {
-        MCMG_NetworkHandler.getLogger().info("LoginEvent detected! {}", e.getResult().getReasonComponent().toString());
-
-    }
-
-    @Subscribe
-    public void onPreLoginEvent(PreLoginEvent e)
-    {
-        MCMG_NetworkHandler.getLogger().info("PreLoginEvent detected! {}", e.getResult().getReasonComponent().toString());
-    }
 
     @Subscribe
     public void onDisconnectEvent(DisconnectEvent e)
     {
-        MCMG_NetworkHandler.getLogger().info("DisconnectEvent detected! {}", e.getLoginStatus().toString());
-
+        if (e.getLoginStatus().equals(DisconnectEvent.LoginStatus.PRE_SERVER_JOIN))
+            initializeNewMainHubServer();
     }
 
+
+    private void initializeNewMainHubServer()
+    {
+        // Get updated information on all network servers
+        CompletableFuture<Void> serverInfoFuture = ActiveServerUtil.getServerInfo();
+
+        // Wait for all server pings to complete, then run remaining code:
+        serverInfoFuture.thenRun(() ->
+        {
+            // Check for any operational main hub servers
+            String serverName = ActiveServerUtil.findTransferableServerName(ServerTypes.MAIN_HUB);
+
+            // If no transferable server could be found, attempt to start a new one
+            if (serverName.isEmpty())
+                ServerInitializeUtil.startNewServer(ServerTypes.MAIN_HUB);
+        });
+    }
 }
