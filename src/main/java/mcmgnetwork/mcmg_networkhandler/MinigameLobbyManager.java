71c374633fa2c420ff5d90624ee8cc44f3674733
mbovero <milesbovero@icloud.com>
2024-05-13 23:36:19 -0600

    Creating class to handle initialization and shutdown of network lobby servers

---
+++ src/main/java/mcmgnetwork/mcmg_networkhandler/MinigameLobbyManager.java
@@ -0,0 +1,14 @@
+package mcmgnetwork.mcmg_networkhandler;
+
+/**
+ * Description: <p>
+ * Responsible for initializing new lobby servers when not enough instances are active and shutting down and deleting
+ * lobby servers when there are an unnecessary number of them.
+ *
+ *  <p>Author(s): Miles Bovero
+ *  <p>Date Created: 5/13/24
+ */
+public class MinigameLobbyManager
+{
+
+}
